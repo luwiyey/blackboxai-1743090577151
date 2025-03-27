4e7045a9c02f36a9862c5ab438e2a4963041644a
luwiyey <159551250+luwiyey@users.noreply.github.com>
2025-03-27 23:51:04 +0800

    Add library_system/accounts/forms.py 1743090664811

---
+++ library_system/accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm
+
+User = get_user_model()
+
+class UserProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'profile_picture']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'profile_picture': forms.FileInput(attrs={'class': 'form-control'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('password')  # Remove password field
