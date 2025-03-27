a1e2400197392d2ad49864bf0dc5fbd2bf701a59
luwiyey <159551250+luwiyey@users.noreply.github.com>
2025-03-27 23:49:41 +0800

    Add library_system/urls.py 1743090581601

---
+++ library_system/urls.py
@@ -0,0 +1,54 @@
+"""
+URL configuration for library_system project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
+    
+    # API Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    # Authentication (Allauth)
+    path('accounts/', include('allauth.urls')),
+    
+    # Apps
+    path('', include('library.urls')),
+    path('user/', include('accounts.urls')),
+    # path('api/', include('api.urls')),  # Temporarily disabled
+    # path('ai/', include('ai_services.urls')),  # Temporarily disabled
+    
+    # Debug Toolbar
+    path('__debug__/', include('debug_toolbar.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
